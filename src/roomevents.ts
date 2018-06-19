5ccd27ddf13c6a8ec6ec28c499c7b56da94ccf1f
Abhishek Kumar <bettiah@gmail.com>
2018-06-19 14:07:42 +0800

    removed un-needed queue watcher

--- src/roomevents.ts
+++ src/roomevents.ts
@@ -1,81 +1,14 @@
-import Redis from 'redis';
-import { duplicateRedis, initRedis } from './redis';
 import {
   Event,
-  MessageEvent,
   MessageEventType,
   StateEventType,
   CreateRoomEvent,
   MessageEventMessgae,
   MemberEvent
 } from './client-server/events';
-import { initDb } from './model';
 import { handleCreateRoom } from './createRoomHandler';
 import { handleMessage } from './messageHandler';
 import { handleMember } from './memberHandler';
-
-const debug = require('debug')('server:events');
-
-interface Hash {
-  [key: string]: any;
-}
-
-// [k1, k2, v1, v2]
-const flatten = (obj: Hash) =>
-  Object.keys(obj).reduce((acc, key) => {
-    acc.push(obj[key]);
-    return acc;
-  }, Object.keys(obj));
-
-const TIMEOUT = 1000000;
-export function roomEvents() {
-  const redis = duplicateRedis();
-  let watching: Hash = { roomevents: '$' }; // using 0 here with empty q is problematic
-  const forever = () => {
-    debug(watching);
-    redis.sendCommand(
-      'XREAD',
-      ['BLOCK', TIMEOUT, 'STREAMS', ...flatten(watching)],
-      async (error, reply) => {
-        Redis.print(error, reply);
-        if (error) {
-          return;
-        }
-        if (!reply) {
-          // no reply means: timed out; setImmediate ensures continuity
-          setImmediate(forever);
-          return;
-        }
-        // synchronously consume repliles
-        // this can be done asynchronously
-        // but then we loose control over number of queued events
-        watching = await processReply(reply as Array<Array<any>>);
-        // continue
-        setImmediate(forever);
-      }
-    );
-  };
-  // start loop
-  forever();
-}
-
-async function processReply(reply: Array<Array<any>>) {
-  let watching: Hash = {};
-  for (const element of reply) {
-    const key = element[0] as string;
-    const values = element[1];
-    for (let timestamped of values as Array<Array<any>>) {
-      const ts = timestamped[0] as string;
-      const [kind, msg] = timestamped[1] as Array<string>;
-      const ret = await processEvent(JSON.parse(msg) as Event);
-      if (!ret) {
-        debug('unable to handle:', key, ts, kind, msg);
-      }
-    }
-    watching[key] = values[values.length - 1][0];
-  }
-  return watching;
-}
 
 export function processEvent(ev: Event) {
   // console.log(key, ts, kind, ev);
@@ -116,10 +49,4 @@ export function processEvent(ev: Event) {
       break;
   }
   return false;
-}
-
-if (require.main === module) {
-  initDb();
-  initRedis();
-  roomEvents();
 }
